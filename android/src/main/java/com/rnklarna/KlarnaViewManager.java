349a0ce82fdd4da6267c72a80131d07153e69db5
Daniil Konoplev <danchoys@icloud.com>
2019-12-25 19:48:00 +0300

    - Move the hierarchy to KlarnaView; - Initialize and deinitialize Klarna correctly; - Vastly simplify the view manager; - Add nullability attributes; - Scroll to top each time a new snippet is srolled;

--- android/src/main/java/com/rnklarna/KlarnaViewManager.java
+++ android/src/main/java/com/rnklarna/KlarnaViewManager.java
@@ -1,70 +1,44 @@
 package com.rnklarna;
 
-import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.common.MapBuilder;
 
-import android.view.ViewGroup;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-
 import java.util.Map;
 
-public class KlarnaViewManager extends ViewGroupManager<LinearLayout> {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
-  private KlarnaView klarnaView;
+public class KlarnaViewManager extends SimpleViewManager<KlarnaView> {
+
   public static final String REACT_CLASS = "RNKlarna";
 
+  @Nonnull
   @Override
   public String getName() {
     return REACT_CLASS;
   }
 
+  @Nonnull
   @Override
-  protected LinearLayout createViewInstance(ThemedReactContext themedReactContext) {
-    // Creating a wrapper instead of a view is needed for correct work of KLarnaCheckout.destroy()
-    klarnaView = new KlarnaView(themedReactContext);
-
-    // The following hierarchy is required for Klarna to work properly:
-    //
-    // <LinearLayout /*container*/> - to wrap the scroll view
-    // ..<ScrollView /*scrollView*/> - because KlarnaView itself is not scrollable
-    // ....<FrameLayout /*contentView*/> - needed otherwise KlarnaView doesn't resize properly
-    // ......<KlarnaView /*view*/> - KlarnaView itself
-    LinearLayout container = new LinearLayout(themedReactContext);
-
-    ScrollView scrollView = new ScrollView(themedReactContext);
-    scrollView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-    container.addView(scrollView);
-
-    FrameLayout contentView = new FrameLayout(themedReactContext);
-    contentView.setLayoutParams(new ViewGroup.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT));
-    scrollView.addView(contentView);
-
-    final View view = klarnaView.getmView();
-    contentView.addView(view);
-
-    return container;
+  protected KlarnaView createViewInstance(@Nonnull ThemedReactContext reactContext) {
+    return new KlarnaView(reactContext);
   }
 
   @ReactProp(name = "snippet")
-  public void setSnippet(View view, String snippet) {
-    klarnaView.setSnippet(snippet);
+  public void setSnippet(KlarnaView view, @Nullable String snippet) {
+    view.setSnippet(snippet);
   }
 
-
-  public Map getExportedCustomBubblingEventTypeConstants() {
-    return MapBuilder.builder()
-            .put(
-                    "onComplete",
-                    MapBuilder.of(
-                            "phasedRegistrationNames",
-                            MapBuilder.of("bubbled", "onComplete")))
+  @Nullable
+  @Override
+  public Map<String, Object> getExportedCustomBubblingEventTypeConstants() {
+    MapBuilder.Builder<String, Object> builder = MapBuilder.builder();
+    return builder.put("onComplete",
+            MapBuilder.of("phasedRegistrationNames",
+                    MapBuilder.of("bubbled", "onComplete")))
             .build();
   }
-
 
 }
